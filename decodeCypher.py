b4a4fcd4d723f37b6d85f7037710293bf1375bfd
Benjamin <benjamin.gaymay@epitech.eu>
2022-07-24 21:37:22 +0200

    upher

--- decodeCypher.py
+++ decodeCypher.py
@@ -4,57 +4,57 @@
 MANGA = [
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/berserk/367/',
+        'https://www.japscan.me/lecture-en-ligne/berserk/367/',
         'cypher':
-        'x8d8ydhoho4oyxz87sco6sjo5jb8jsxo9h586o48ysbozshohhbocs5odddox85o4jooz84886hoc9zoy9coj67oj8z/8ss5ocobox6so9xyoo8x84y7ozocohh7ocoho9jcocsjo8h6o8jjocj58osj8995o8ybozpyocsd8hxoyxsdydjyyhh/78cvz8dyx8jdj8yody4m5ooys8yvj8o6sy6vcx6hyoh66ocmxxxyhoz8yozo5ohmyxyuxj8x9sxsdjyohshx4syszj7/xcj6mjx',
+        '8fr4hfvssddscd1scfpsc2p42d5s1vrsrdcscphsc2h452nsrhcs3p2sdd3svhp4chf4hhds3rr4hsps5shs544414c/sd48sh21s34vs8454sdbs1fhs3sh44fh43d1snrn4dhh4dcds8srssf3snrh45sfs8fr4n03sdfs48h3chfpcr2dcnr/s4ptv4scp45vs4hsfc36hsvc14ftb42d1c4t3h8rfscdds46phbccs148s4s3s86nhvl42nhcf8fr2hsvfshdfcf12c/hp2c6ph',
         'decoded':
-        '9363468080d049f3c2e07210a1b3129058a370d342b0f28088b0e2a0666093a0d100f3d33780e5f045e017c013f/322a0e0b0972059400393d4c0f0e088c0e08051e0e21038703110e1a3021355a034b0fl40e26389049264614488/c3emf3649316134064dna004234m1307247me978408770en999480f340f0a08n494j9139529261408289d242f1c/9e17n19'
+        'f0617048855825b82038293195a8b468652823782971a9c86728d39855d8473127017758d6617838a878a111b12/851f879b8d148f1a185e8b078d8711071d5b8c6c157715258f86880d8c671a808f061cld85081f7d270326952c6/813m418231a4817802dn7842b10me195b21md7f60825581n37e228b1f818d8fnc74j19c720f0697840875020b92/7392n37'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/isekai-apocalypse-mynoghra-the-conquest-of-the-world-starts-with-the-civilization-of-ruin/16/',
+        'https://www.japscan.me/lecture-en-ligne/isekai-apocalypse-mynoghra-the-conquest-of-the-world-starts-with-the-civilization-of-ruin/16/',
         'cypher':
-        'sdoj99zhoyzhxy7h5jhjoshhco4hzosjd67h9hsjojyhodhh7ojhzxcj867h5ysh6hshy8chhhjh4y5h8dxhsj4hbdbjojyjjhshs98h7h8h5dzhhs5hh99hzyjh7x8hc97h5objcxshd84h9dxhzsdho69j7o4jb1hhbo7j964ssoosxxos8dsjcl8j8syjjybjjhh/szgyhss4jo9dsbqsj9ly6odyhs9yhbgy65jjh7d7hhgs64l4x6y8xooyos6oo91z6ojzxxg7x51h6olhxcg4xzysxdx7o9dsxoqyooyhx70jxzycx8j7o5ysx6hsxy1c6hojx485x8xxos146b8bxoxyojysxss8o71865hzohx5oh69ozdjx7x8och7o51b6cxsxd0/4x96xozgdxos9o7146bohxb87x9x4os1o6xjox8gsxc1868xyojhbojohxzoyosd4ooldxbgsx9hyxo1y6soyxb8yx5xjo7176hdsx464o688xosyxsgox9lzxoyzox17x51h6oohxch4oz9sody7o90sxodyxohhx7gjxz9co8d7x51s66ysxydcxhijx4o5x8hxos/y4xbgb6',
+        'crf4dd8sfv8s2vns54s4fcssbf3s8fc4rhnsdsc4f4vsfrssnf4s82b4phns5vcshscsvpbsss4s3v5spr2sc43s1r14f4v44scscdpsnsps5r8ssc5ssdds8v4sn2psbdns5f14b2csrp3sdr2s8crsfhd4nf341xss1fn4dh3ccffc22fcprc4bap4pcv44v144ss/c8gvscc34fdrc1qc4davhfrvscdvs1gvh544snrnssgch3a32hvp2ffvfchffdx8hf4822gn25xshfas2bg328vc2r2nfdrc2fqvffvs2ny428vb2p4nf5vc2hsc2vxbhsf423p52p22fcx3h1p12f2vf4vc2ccpfnxph5s8fs25fshdf8r42n2pfbsnf5x1hb2c2ry/32dh2f8gr2fcdfnx3h1fs21pn2d23fcxfh24f2pgc2bxphp2vf4s1f4fs28fvfcr3ffar21gc2dsv2fxvhcfv21pv2524fnxnhsrc23h3fhpp2fcv2cgf2da82fv8f2xn25xshffs2bs3f8dcfrvnfdyc2frv2fss2ng428dbfprn25xchhvc2vrb2s9423f52ps2fc/v321g1h',
         'decoded':
         '260155f804f894c8a1810288e0d8f02167c8582101480688c018f9e137c8a428782843e88818d4a8369821d8b6b1014118282538c838a6f882a88558f418c938e5c8a0b1e92863d85698f2680751c0d1bx88b0c157d2200299023621ey31324114b1188/2fz4822d10562bu215y4706482548bz47a118c6c88z27dyd97439004027005xf701f99zc9ax870y89ezd9f42969c056290u400489cw19f4e931c0a42978294xe78019d3a939902xd7b3b9094014292230cx37a8f089a08750f619c930e8c0axb7e9296w/d95790fz690250cxd7b089b3c959d02x0791093z29ex37394018b01089f04026d00y69bz2958490x472049b349a910cxc78629d7d0733902492z095yf904f09xc9ax870089e8d0f52064c05w2906490889cz19f5e036c9ax27742946e98v19d0a938902/4d9bzb7'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/one-punch-man/224/',
+        'https://www.japscan.me/lecture-en-ligne/one-punch-man/224/',
         'cypher':
-        'hs7jdo5ycx4y668jy8xyxhsj4xxjy6hj9ooy5xcy9s8j56bjdhbj6sxjyozyhx4jbs4yjydj9xjyxxsj7yxy8doj584jcs9y7hb/jzszy78yyy8dyod4jy64j9o5j48xjd85y4ydyhsojcjsy9dxjdj6j9s5j5o7jouhj888y4oc9z8b97sj98xoyy2oyj98y967y6j/59zp8jb95y824yshb992codhbjyhhjspsodssjoobjzozj8pxo628soxbshu7sdu5ocd4s6j8sy2xsx8s84hx8yuho98os52cs9/p8s5pbo',
+        'scn4rf5vb23vhhp4vp2v2sc43224vhs4dffv52bvdcp45h14rs14hc24vf8vs2341c3v4vr4d24v22c4nv2vprf45p34bcdvns1/48c8vnpvvvprvfr34vh34df543p24rp5v3vrvscf4b4cvdr24r4h4dc545fn4fls4pppv3fbd8p1dnc4dp2fvvjfv4dpvdhnvh4/5d80p41d5vpj3vcs1ddjbfrs14vss4c0cfrcc4ff148f84p02fhjpcf21cslncrl5fbr3ch4pcvj2c2pcp3s2pvlsfdpfc5jbcd/0pc501f',
         'decoded':
         '82c160a4e9d4773143949821d99147815004a9e45231a7b168b1729140f489d1b2d4146159149921c4943601a3d1e254c8b/1f2f4c344436406d147d150a1d39163a4d4648201e1245691617152a1a0c10j813334d0e5f3b5c21539044k04153457c471/a5fl31b5a43kd428b55ke068b148812l20622100b1f0f13l907k3209b28jc26ja0e6d271324k929323d8934j805302ake25/l32alb0'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/hajime-no-ippo/1386/',
+        'https://www.japscan.me/lecture-en-ligne/hajime-no-ippo/1386/',
         'cypher':
-        'd6ho66hoh8o8joyox69o6s4o486o8j7o9yzo8s98d8d86jooh8x8768oc848hyzo49yo6jc8xjdo5dzohdcoxdzo4ho8xhyozooozo/78zd4oso58hoc8oxy84s8o5oj87jsobhdo98ooxoh8d9oohdhoz8do76oojnbo8sh8oxyyjs5y5j4y9hh8zr889yx8bdh88oyy639o/by785r9856xycroxc66o764oo3bxj88o894o9oooyhxo93jx43yjd6hs66hshdojjnyxx39j6r4j4n6x8s7j9nzj8d9jdedj6hojh9/xj738x',
+        'rhsfhhsfspfp4fvf2hdfhc3f3phfp4nfdv8fpcdprprph4ffsp2pnhpfbp3psv8f3dvfh4bp24rf5r8fsrbf2r8f3sfp2svf8fff8f/np8r3fcf5psfbpf2vp3cpf5f4pn4cf1srfdpff2fsprdffsrsf8prfnhff4w1fpcspf2vv4c5v543vdssp8ippdv2p1rsppfvvhedf/1vnp5idp5h2vbif2bhhfnh3ffe124ppfpd3fdfffvs2fde423ev4rhschhscsrf44wv22ed4hi343wh2pcn4dw84prd4rkr4hsf4sd/24nep2',
         'decoded':
         '6780778083031040975072d0d37031c054f03253636371008393c730e3d384f0d54071e39160a6f086e096f0d8039840f000f0/c3f6d020a380e30943d230a013c120b8605300908365008680f360c7001rb03283094412a4a1d45883fs335493b68330447t50/b4c3as53a794es09e770c7d00tb9133035d0500048905t19dt416782778286011r499t517sd1dr7932c15rf136516o61780185/91ct39'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/tales-of-demons-and-gods/386.5/',
+        'https://www.japscan.me/lecture-en-ligne/tales-of-demons-and-gods/386.5/',
         'cypher':
-        'o84ozjzo5h7oyx9858s8ho7ody6o4o9ozozojybods8o4xh869sozxhos9ooxobo98o85868js4oxxz87d6o9obo5ohohj58sd7ozhbo68zocs6o6jyo9/hh84s684o5ocshocsj84ycozd8ozjcoy8h88j8odsj85uyohsx8cx5yzs6y9jzy7h7872o8xyj85do8socyyp5o4yj8s6jyjfj8d2zxohxo86zo5phxbs/zo92yx98coy9zooododpzxoo4szjzj5p7jyy9j5usxhj7jd26j4h9jzuzxjobsd28j4phj6usjzshjsjojxubx98oj5p6jju4xxozs7s6j9abj5hhjhj5/ssp7x',
+        'fp3f848f5snfv2dp5pcpsfnfrvhf3fdf8f8f4v1frcpf32sphdcf82sfcdff2f1fdpfp5php4c3f228pnrhfdf1f5fsfs45pcrnf8s1fhp8fbchfh4vfd/ssp3chp3f5fbcsfbc4p3vbf8rpf84bfvpspp4pfrc4p5lvfsc2pb25v8chvd48vnsnpnjfp2v4p5rfpcfbvv05f3v4pch4v474prj82fs2fph8f50s21c/8fdjv2dpbfvd8fffrfr082ff3c848450n4vvd45lc2s4n4rjh43sd48l824f1crjp430s4hlc48cs4c4f42l12dpf450h44l322f8cnch4dz145ss4s45/cc0n2',
         'decoded':
         '03d0f1f0a8c04953a32380c06470d050f0f014b06230d9837520f980250090b05303a37312d099f3c67050b0a08081a326c0f8b073f0e27071405/883d273d0a0e280e213d4e0f630f1e0438331306213aj408293e9a4f27451f4c8c3ck039413a60320e44la0d41327141g136kf9089037f0al89b2/f05k4953e045f000606lf900d2f1f1alc14451aj2981c16k71d851fjf910b26k31dl817j21f28121019jb95301al711jd990f2c2715ib1a88181a/22lc9'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/mashle/114/',
+        'https://www.japscan.me/lecture-en-ligne/mashle/114/',
         'cypher':
-        'jjxd56o8xd78b6489x485jjdjjz8hy98yss8zd7d66h8z97879685dc8cssd68z84dj8j96dcyddzy78ood87j8db9/od7sj8os4d6j68689879yd7sj8js5dhj4dz9688ho86oj8d6c8ojb8ojd8odc8sdzd9h98hwz8x94dysb64996xys6/6jbdxa6dh6cdzx8dh8764u7856odjoz6hcbdxahszjb8hod86uosbyo8yjh8zjz8bu5sd9hyzf7yjhxy58o9xj7ybw/4y9u4s',
+        '442r5hfp2rnp1h3pd23p544r448psvdpvccp8rnrhhsp8dnpndhp5rbpbccrhp8p3r4p4dhrbvrr8vnpffrpn4pr1d/frnc4pfc3rh4hphpdpndvrnc4p4c5rs43r8dhppsfphf4prhbpf41pf4rpfrbpcr8rdsdpsm8p2d3rvc1h3ddh2vch/h41r2zhrshbr82prspnh3lnp5hfr4f8hsb1r2zsc841psfrphlfc1vfpv4sp848p1l5crdsv87nv4s2v5pfd24nv1m/3vdl3c',
         'decoded':
         '1196a70396c3b7d359d3a11611f384534223f6c67783f5c3c573a6e3e22673f3d6131576e466f4c30063c136b5/06c21302d671737353c546c21312a681d6f5733803701367e301b3016306e326f658538hf395d642b7d5579427/71b69i7687e6f93683c7djc3a70610f78eb69i82f1b380637j02b4034183f1f3bja26584fgc41894a30591c4bh/d45jd2'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/tokyo-revengers/260/',
+        'https://www.japscan.me/lecture-en-ligne/tokyo-revengers/260/',
         'cypher':
-        'h89yjxsydjyjd9ojxx7ycd4jhszjoo8yc9bjsy9jz6zjbh8j5sojojxysj6yzxyyjx4yhh7j4x4jxhbjo86yysdjdshjdjyj5oyj6j/5ybjby5j5yc8yjsscjzy5j4shycjbj9o9yjscyyj6jhs5johcjcj8j9xbyhnojh8cy4oy968c9bso97xoy4rby59cyb6yy7j99537j/y9dyjh695e9y7r4o8xdjhh6jd3bo4hdjby8jbo6j63boxjb8so48h89sj9ssdrysd8osxy78c84shoz8on8oc3bss698ztzsb38s5s/o8o3xo',
+        'spdv42cvr4v4rdf422nvbr34sc84ffpvbd14cvd48h841sp45cf4f42vc4hv82vv423vssn432342s14fphvvcr4rcs4r4v45fv4h4/5v141v545vbpv4ccb48v543csvb414dfdv4cbvv4h4sc54fsb4b4p4d21vswf4spbv3fvdhpbd1cfdn2fv3i1v5dbv1hvvn4dd5en4/vdrv4shd5kdvni3fp2r4ssh4re1f3sr41vp41fh4he1f241pcf3pspdc4dccrivcrpfc2vnpbp3csf8pfwpfbe1cchdp8u8c1epc5c/fpfe2f',
         'decoded':
         '835419246141650199c4e6d182f10034e5b12451f7f1b831a20101942174f94419d488c1d9d198b10374426162816141a04171/a4b1b4a1a4e34122e1f4a1d284e1b1505412e4417182a108e1e13159b48r0183e4d04573e5b205c904dsb4a5e4b744c155atc1/45641875ao54csd0396188716tb0d861b431b0717tb091b320d3835215226s42630294c3e3d280f30r30etb22753fpf2bt32a2/030t90'
     },
@@ -63,65 +63,49 @@ MANGA = [
 WEBTOON = [
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/solo-max-level-newbie/54/',
+        'https://www.japscan.me/lecture-en-ligne/solo-max-level-newbie/54/',
         'cypher':
-        'do22ddka8odaa21ak2u28hg2wwrar2d2bdaahas2wouarokah5gaqrbaqw5abo825ogaghaadk1awaa2hwhao5qa1hoara82s1gashra8w1ah/k2aqhoahwhagrr2wowad5rauak2u5kaghk25hrau2raukra5wraqmqahho2wdskuhwkarakgoq2rpo2bkg2sw52haakulaaakh2b1hkq352qp/hdgokau18aulkd2r1akhrawlgdshgrbwur2dordk2hdhkr8pdram1dk6ur8hgrw2rhrhdrb6arhmsdw5uhrokrhmgrqmbdql5rb68r5lgrgaa/hdl1d',
+        '0oss005t7o0ttsct5sus7hnsllktks0sq0tthtmsloutko5th6ntfkqtfl6tqo7s6ontnhtt05ctlttshlhto6ftchotkt7smcntmhkt7lcth/5stfhothlhtnkkslolt06ktut5su65tnh5s6hktusktu5kt6lktf2fthhosl0m5uhl5tkt5nofsk3osq5nsml6shtt5u8ttt5hsqch5fw6sf3/h0no5tuc7tu850skct5hktl8n0mhnkqluks0ok05sh0h5k730kt2c05vuk7hnklskhkh0kqvtkh2m0l6uhko5kh2nkf2q0f86kqv7k68nkntt/h08c0',
         'decoded':
         '21662273a123360376e6a5f699434626b23353d691e3417358f3c4b3c983b1a681f3f53327039336595318c3051343a6d0f3d543a9035/763c5135953f44691932843e376e873f5768543e643e7438943clc3551692d7e5973437f1c64m16b7f6d9865337en333756b057ci86cm/52f173e0a3en72640375439nf2d5f4b9e4621427652574am243l027ke4a5f496454524bk345ld298e541745lf4clb2cn84bka48nf4f33/52n02'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/return-of-the-female-knight/44/',
+        'https://www.japscan.me/lecture-en-ligne/return-of-the-female-knight/44/',
         'cypher':
-        'ohrowdqrok5oow5rghaor1hr8dkrqk8ow1wrsworqrqrkaaou58ok2ho8kwo1ohrqrbrbakosaqodogrkdbrqd5r81ao85uodwgokr2oaorrkkqoaahos/oqoha8o21hogr2odrarbodoqd2raodo8ogr11dogkqooerokahrg1wh8a2hsdoh8w2r165rwhwrgkorgohhgm8oohqr15hhuz1rd6s1dw1oq5wokmb1od/oobdwoqm51wd1ak2kdoh2dokrdw6qdo95doe51gaadr3hd8wkdqe8dwawdsrodqeq1kaadu28dkdha8ew11rhdqmbdbek1s6qdd1gakabaqd5a8aad81u/admg1',
+        'ohkol0fko56ool6knhtokchk705kf57olclkmlokfkfk5ttou67o5sho75locohkfkqkqt5omtfo0onk50qkf06k7cto76uo0lno5ksotokk55fotthom/ofoht7oschonkso0ktkqo0of0skto0o7onkcc0on5foo9ko5thknclh7tshm0oh7lskcv6klhlkn5oknohhn27oohfkc6hhuack0vmc0lcof6lo52qco0/ooq0lof26cl0ct5s50ohs0o5k0lvf0od60o96cntt0kwh07l50f970ltl0mko0f9fc5tt0us7050ht79lcckh0f2q0q95cmvf00cnt5tqtf06t7tt07cu/t02nc',
         'decoded':
         '154192c417811984f5314054a274c7a19094d914c4c47331e8a17651a7910154c4b4b371d3c121f472b4c284a031a8e129f17461314477c13351d/1c153a16051f4612434b121c2643121a1f40021f7c11j417354f095a365d215a9640k849594f714f155fla115c40855eg042kd02901c8917lb012/11b291cl8092037672156217429kc21h821j80f3324i52a972cja29392d412cjc07332e6a27253aj900452clb2bj70dkc220f373b3c283a332a0e/32lf0'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/ranker-s-return-remake/37/',
+        'https://www.japscan.me/lecture-en-ligne/ranker-s-return-remake/37/',
         'cypher':
-        'bhah5wbdkashbkaddhwduwsd1radsrrhha2h8rqh8hqhdaqhdrah81qdwokdaosdgkkdwrwhdard2w8dqwdd2h8ds1uh8khd1w2d212hr2adka/ghgrrhbwadbkbdkowd5hkhq1rdg15h152dqo8hb5qdhh1dskwd5vhd2rghoou2krk2rag211wh87aho25hg5ghkdh2dyodw2dhwwu2dp2hh75o/s1qdawudryaor2qd5dbdbyso1r8abl1a51kabvaa5rbakosrbvaod7wauosr1rarsarrhr2a8oqr8vqoddqrdvao8oqrwrkaalsag7kaw1wado/rr2y8o',
+        'qhth6lq05tmhq5t00hl0ulm0ckt0mkkhhtsh7kfh7hfh0tfh0kth7cf0lo50tom0n550lklh0tk0sl70fl00sh70mcuh75h0cls0scshkst05t/nhnkkhqlt0q5q05ol06h5hfck0nc6hc6s0fo7hq6f0hhc0m5l06eh0sknhoous5k5sktnscclh71thos6hn6nh50hs0io0ls0hllus03shh16o/mcf0tlu0kitoksf060q0qimock7tq8ct6c5tqett6kqt5omkqeto01ltuomkcktkmtkkhkst7ofk7efo00fk0eto7ofklk5tt8mtn15tlclt0o/kksi7o',
         'decoded':
         'b53589b273d5b7322592e9d20432d4455365a4c5a5c523c52435a0c2917231d2f7729495234269a2c92265a2d0e5a75209626065463273/f5f445b932b7b271928575c042f0850862c1a5b8c25502d7928p5264f511e6747643f60095aq351685f8f572562r12962599e62m655q81/d0c239e24r3146c282b2brd104a3bn038073bp3384b371d4bp312q93e1d40434d3445463a1c4apc122c42p31a1c494733nd3fq73909321/446ra1'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/ranker-s-return-remake/37/',
+        'https://www.japscan.me/lecture-en-ligne/gosu/198/',
         'cypher':
-        'wouwuhbwkdkwrwkwkwhwro2wuk2w128wad1w81qw2hdwdwgwa1odaw2do5oddkrd8haw5oddgwbwaw1whhowhwhwu1/qwbo8w2kuduokdh2uw5hgwqh5wgdqwu18w2dwdh1uddwaddkww2wsdg5hwrawwhkqdwwqdg9owsogdw5gaooqa21sa/kkbd53odgakdshadrwsaue8wraadg2wa5uhdb3o58kwwh2bwwek5dhrw5abwbeo5k5s1w9b5hob1wduoukb1kzk1rw/kokeh5',
-        'decoded':
-        '91e9e5b97279497979594169e76906a93209a0c9652929f93012396218122742a5398122f9b9390955195959e0/c9b1a967e2e17256e985f9c589f2c9e0a9629250e22932279969d2f859439957c299c2fh19d1f298f311c360d3/77b28i12f372d53249d3eja94332f6938e52bi18a79956b99j78254983b9bj1878d09hb851b092e1e7b07g7049/717j58'
-    },
-    {
-        'url':
-        'https://www.japscan.ws/lecture-en-ligne/gosu/198/',
-        'cypher':
-        'so51g2w1u5h18wgak2k1wwka1wuarow111r1bk81h281baga8ho1h1r15rs1a2g12d2aurr1krh15wqak2h1oos/abdgaq221ook18kw12a8arhs1oada82r1o1oago11aas1kd1aaa512wgaoa1155d185kakdgadfu1kdgauw5rhd/drdo5r25da185asrga22qao15robu12r8a8kur8xsa88awdk11gkr1hbkwsha1h2d115h1gb5wodsdg1sdsb5og/rwoubhw',
+        'mo6cnslcu6hc7lnt5s5cll5tclutkolccckcq57chs7cqtnt7hochckc6kmctsncs0stukkc5khc6lft5shcoom/tq0ntfsscoo5c75lcst7tkhmcot0t7skcocotnoccttmc50cttt6cslntotcc660c765t50nt0yuc50ntul6kh0/0k0o6ks60tc76tmkntssftoc6koqucsk7t75uk7zmt77tl05ccn5kchq5lmhtchs0cc6hcnq6lo0m0ncm0mq6on/klouqhl',
         'decoded':
         'd180f690e850a9f37670997309e341900040b7a056a0b3f3a510504084d036f06263e440745089c3765011d/3b2f3c6601170a79063a345d01323a6401013f10033d07203338069f313008820a87372f32ze072f3e98452/24218468230a83d4f366c310841be064a3a7e4awd3aa392700f7405b79d53056200850fb8912d2f0d2db81f/491eb59'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/gosu/198/',
+        'https://www.japscan.me/lecture-en-ligne/invincible-at-the-start/10/',
         'cypher':
-        '1kwawd125aw2rh82sadaqh125a8a5auaarda52h2gooakk1a5o1aua1audgad1uauaa2b2o2gr1aaahas5aaaagagwrabrbahrq25/daaskrakoda828addk2k5ga2rha2rgagau2dwwasa5adru25aa2adoaghu2rluaqh822dwkbhukrr2kdos2ujh2aka28wg22agk8v/qakkd2d1hku6r2kjsdaoqad1qawvadqwdawa5abv1dowdhgaurbjrr1owrwl1r5vwrrd8hsadrqh1r5j8r5ludavdr5phrgvorka1/h5v1d',
-        'decoded':
-        '07939206839645a6d323c50683a383e334238656f11377038103e303e2f320e3e336b616f4033353d83333f3f943b4b354c68/233d7437123a6a3227678f3645364f3f3e62993d38324e683363213f5e64ne3c5a66297b5e7446721d6eo563736a9f663f7ap/c377262057ek467od231c320c39p32c9239383bp021925f3e4bo4401949n048p9442a5d324c5048oa48ne23p248m54fp14730/58p02'
-    },
-    {
-        'url':
-        'https://www.japscan.ws/lecture-en-ligne/invincible-at-the-start/10/',
-        'cypher':
-        'kwbd5a8dr52dbouhw5odbdod52kduh1hkaoh5aadhabd5akddduhaa8hrhhd5o8dsrhhd5gd8ksdqwodga8hu1odadahud1da25drh1db5hdohq/hbood1owds12d21kdahkd5akdwaqdb2rd8r1h1kbdh1dd5drhhikdor5hsor2kr12aao221hhac2hk2qho52hrdq2st5da2ahsws2k7bh2coos1/qdgw2dot2okw1dg1hd5twoqagrsosru1bakabr5d8rri2obruawkoabaor5ikoua1rk1oa5iaohrba54kadouaa58ardha5c8as5hadhgr8csaq/5oagt8o',
+        '5lq06t70k6s0qouhl6o0q0o06s50uhch5toh6tt0htq06t5000uhtt7hkhh06o70mkhh06n075m0flo0nt7huco0t0thu0c0ts60khc0q6h0ohf/hqoo0col0mcs0sc50th506t50ltf0qsk07kchc5q0hc0060khhp50ok6hmoks5kcsttosschhtbsh5sfho6shk0fsmj60tsthmlms51qhsboomc/f0nls0ojso5lc0nch06jloftnkmomkucqt5tqk607kkpsoqkutl5otqtok6p5outck5cot6ptohkqt6r5t0outt67tk0ht6b7tm6ht0hnk7bmtf/6otnj7o',
         'decoded':
         '79b283a24862b1e59812b2128672e5057315833253b2837222e533a5455281a2d45528f2a7d2c912f3a5e0123235e20236824502b85215c/5b1120192d06260723572837293c2b642a40507b2502282455t721485d1467406331660553u6576c5186542c6dv823635d9d67qb56u11d0/c2f9621v617902f0528v91c3f4d1d4e0b373b482a44t61b4e39713b3148t71e30470138t3154b38s7321e338a342538ua3d85325f4aud3c/813fva1'
     },
     {
         'url':
-        'https://www.japscan.ws/lecture-en-ligne/library-of-heaven-s-path/60/',
+        'https://www.japscan.me/lecture-en-ligne/library-of-heaven-s-path/60/',
         'cypher':
-        'r5o51125dhg522d5okdo12d5sorod21o8wuoqk1okau5bdu5ao25r5b5arh52k25sow5qau51kkod2b51ok5wad5ahu55rq52w2ok2r521woqda52/k55g1ko2a15d555k5h5k5s5bor5b51585o52wuoq115b5ro50w5u1kogksd11qdkwhdo2oo8naoadhoaroos5qdrf255dwo8had8c5ounwk82q58h/552fsk5h25bo25dfbk5a2w1w11q25wrho1102kd5g120dkondw11dwsor1d21w81uwqa1wk0ukbouwaf2wr0bkarh12k21s2wwqku11kkwdrbw1xk/wwfdk',
+        'k6o6ccs60hn6ss06o50ocs06moko0sco7luof5co5tu6q0u6tos6k6q6tkh6s5s6mol6ftu6c55o0sq6co56lt06thu66kf6slso5sk6sclof0t6s/566nc5ostc6066656h656m6qok6q6c676o6sluofcc6q6ko6xl6uc5on5m0ccf05lh0osoo7gtot0hotkoom6f0kys660lo7ht07b6ougl57sf67h/66sym56hs6qos60yq56tslclccfs6lkhoccxs506ncsx05og0lcc0lmokc0scl7culftcl5xu5qoultyslkxq5tkhcs5scmsllf5ucc55l0kqlcz5/lly05',
         'decoded':
         '4818006825f8662817210628d1412601a9e1c70173e8b2e8316848b834586768d198c3e8077126b80178932835e884c8696176486091c2386/788f07163082888785878d8b148b808a81869e1c008b8418x98e071f7d200c279521611ay3132513411d8c24z688291a532au81ey97a6c8a5/886zd78568b1682zb783690900c68945100x6728f06x271y290029d1402609a0e9c3097xe7b1e93z694xb734506760d699c7e0077924b90w7/99z27'
     },
