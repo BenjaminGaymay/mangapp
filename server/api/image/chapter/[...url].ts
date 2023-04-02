5866b9326b5c3de39f2c5ab36a71c6d700a36244
Benjamin <benjamin.gaymay@epitech.eu>
2023-04-02 11:57:49 +0200

    add referer

--- server/api/image/chapter/[...url].ts
+++ server/api/image/chapter/[...url].ts
@@ -4,7 +4,8 @@ export default defineEventHandler(async (event): Promise<any> => {
 	const url: string = event.context.params?.url || '';
 
 	const response = await cloudscraper.get(`https://c.japscan.lol/${url}`, {
-		timeout: { request: 60000 }
+		timeout: { request: 60000 },
+		headers: { referer: 'https://www.japscan.lol/' }
 	});
 
 	if (!response) return null;
